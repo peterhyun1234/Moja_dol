ffb7a76ce1c1f7c6f950cf64e9617d09f6e34aff
peter <peterhyun1234@gmail.com>
2019-09-29 18:26:40 +0900

    add DB infomations

---
+++ backend/config/database.js
@@ -0,0 +1,25 @@
+module.exports=(function(){
+    
+    return{
+        local:{
+            host:'localhost',
+            user:'HB_JEON',
+            password:'page159!',
+            port:3306,  
+            database:'mojadol_DB00'
+        },
+        
+        real:{
+            host:'',
+            user:'',
+            password:'',
+            database:'mojadol_DB00'
+        },
+        dev:{
+            host:'',
+            user:'',
+            password:'',
+            database:'mojadol_DB00'
+        }
+    }
+})();

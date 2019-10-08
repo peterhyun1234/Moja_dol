9cd2f7845d7d592fb19a7415cee39cd8ee42e436
ncavchl <ncavchl@ajou.ac.kr>
2019-10-08 21:57:19 +0900

    web_front 생성

---
+++ web_front/webMojadol/js/member.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	
+    var monkeyList = new List('test-list', {
+        valueNames: ['member'],
+        page: 3,
+        pagination: true
+      });
+});
---
+++ web_front/webMojadol/server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/views/login.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200/*, {'Content-Type': 'text/html; charset=utf-8'}*/);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+
+
+app.listen(3000);

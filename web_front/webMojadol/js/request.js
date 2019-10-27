2fc8f49ec9485d5302360029496bf65276a04956
ncavchl <ncavchl@ajou.ac.kr>
2019-10-27 23:43:08 +0900

    회원 관심사 목록 기능 구현

--- web_front/webMojadol/js/request.js
+++ web_front/webMojadol/js/request.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+	$.ajax({
+		url: "http://49.236.136.213:3000/request/show_all_reqs",
+		type: "POST",
+		success: function(data){
+		   //alert('성공');
+			$.each(data, function(idx, content){
+				//alert(idx + ":"+content.name);
+				var string = '<li class="requestlist">'+
+							 '<span class="req_code" data-sort="req_code">'+ content.req_code +'</span>'+
+							 '<span class="userID" data-sort="userID">'+ content.req_uID +'</span>'+
+							 '<span class="req_category" data-sort="req_category">'+ content.req_category +'</span>'+    
+							 '<span class="request_time" data-sort="request_time">'+ content.req_time +'</span>'+   
+							 '<span class="request_comment" data-sort="request_comment"><input type="text" value="' +content.req_contents +  '" readonly/></span>'+   
+							 '<div class="hover_commentBox"><div class="hover_comment">' +content.req_contents +  '</div></div>';
+
+				if(content.req_flag == null){
+					var flag = '<span class="request_flag" data-sort="request_flag"><input type="checkbox" checked data-toggle="toggle"></span></li>';
+				}
+				else {
+					var flag = '<span class="request_flag" data-sort="request_flag"><input type="checkbox" unchecked data-toggle="toggle"></span></li>';
+				}             
+				
+				string = string + flag;
+				$(".list").append(string);
+
+			});
+			
+		},
+		error: function(){
+			alert("데이터베이스 에러");
+		}
+	});
+
+
     portfolioPageSetting();
     hover_request_comment();
 });

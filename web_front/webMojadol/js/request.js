f7c5814e2da3c32784c0f77b0046847b7f346e02
ncavchl <ncavchl@ajou.ac.kr>
2019-10-28 22:29:44 +0900

    사용자 요청관리 서버 연동 완료 - 정렬,리스트 오류

--- web_front/webMojadol/js/request.js
+++ web_front/webMojadol/js/request.js
@@ -6,19 +6,28 @@ $(document).ready(function(){
 		   //alert('성공');
 			$.each(data, function(idx, content){
 				//alert(idx + ":"+content.name);
+				//timestamp 전처리
+				var beforedate = content.req_time;
+				var cut = beforedate.split("T");
+				var date = cut[0];
+				var cut2 = cut[1].split(".000Z");
+				var time = cut2[0];
+				var retime = date + " " + time;
+				//alert(retime);
+
 				var string = '<li class="requestlist tt">'+
 							 '<span class="req_code" data-sort="req_code">'+ content.req_code +'</span>'+
 							 '<span class="userID" data-sort="userID">'+ content.req_uID +'</span>'+
 							 '<span class="req_category" data-sort="req_category">'+ content.req_category +'</span>'+    
-							 '<span class="request_time" data-sort="request_time">'+ content.req_time +'</span>'+   
+							 '<span class="request_time" data-sort="request_time">'+ retime +'</span>'+   
 							 '<span class="request_comment content" data-sort="request_comment"><input type="text" value="' +content.req_contents +  '" readonly/></span>'+   
 							 '<div class="hover_commentBox"><div class="hover_comment">' +content.req_contents +  '</div></div>';
 
 				if(content.req_flag == null || content.req_flag == 0){
-					var flag = '<span class="request_flag" data-sort="request_flag"><input type="checkbox" id="test" unchecked data-toggle="toggle" onclick="modifyflag(this.id)"></span></li>';
+					var flag = '<span class="request_flag" data-sort="request_flag"><input type="checkbox"  unchecked data-toggle="toggle" onclick="modifyflag(this.id)" id="'+"request"+content.req_code+'"></span></li>';
 				}
 				else {
-					var flag = '<span class="request_flag" data-sort="request_flag"><input type="checkbox" id="test" checked data-toggle="toggle"  onclick="modifyflag(this.id)"></span></li>';
+					var flag = '<span class="request_flag" data-sort="request_flag"><input type="checkbox"  checked data-toggle="toggle"  onclick="modifyflag(this.id)" id="'+"request"+content.req_code+'"></span></li>';
 				}             
 				
 				string = string + flag;
@@ -34,7 +43,7 @@ $(document).ready(function(){
 
 
     portfolioPageSetting();
-    hover_request_comment();
+
 });
 //req_code userID req_category request_time request_comment request_flag
 function portfolioPageSetting(){
@@ -44,73 +53,40 @@ function portfolioPageSetting(){
 			valueNames: [
 				'req_code','userID','req_category', 'request_time', 'request_comment', 'request_flag'    // 1.검색을 위한 영상의 제목
 			],
-			page: 6,			 // 한 페이지 출력 영상 개수 
+			page: 3,			 // 한 페이지 출력 개수 
 			pagination: true	 // 자동 페이징 여부 
 		};
 	
 		var userList = new List('test-list', options); // 위 옵션에 따라 만들어진 리스트
-		
+	
 		//list 받아오는 ajax 적기 
 }
 
-function hover_request_comment(){
-
-}
 
 function modifyflag(me){
-	//alert("체크 누름" + me + "여부"+ $(me).is(":checked"));
-
-
+	var cut = me.split("request");
+	var req_code = cut[1];
+	var req_flag;
 	var string = "input:checkbox[id='"+me+"']";
-	var recode = $(me).parent().html();
-	alert(recode + "코드");
+	//alert(me + "여부" + string);
 
-	if($(string).is(":checked") == ture){
-		// 원래는 false 였음
-		/* $.ajax({
-			url : "http://49.236.136.213:3000/request/change_flag",
-			type : "post",
-			data : admin,
-			success : function(data) {
-				
-				if(data == 1){
-					alert("관리자 로그인에 성공하였습니다.");
-					$(location).attr("href", "member.html");
-				}
-				else {
-					alert("로그인 정보를 확인해주십시오." + data)
-				}
-				
-			},
-			error: function(request,status,error){
-				alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
-			}
+	if($(string).is(":checked") == true) req_flag = 1; //원래 노체크였으면 1로해서 보냄(누르는 순간 checked로 되어 true로 출력되는 것임)
+	else req_flag=0; // 원래 체크였으면 0으로 해서 보냄
 	
-		});  */
-	}
-	else {
-		//원래는 true 였음
-	}
+	var request = {"req_code" : req_code, "req_flag" : req_flag};
 
-	/*$("input:checkbox[id='ID']").is(":checked") == true : false */
-/* 	$.ajax({
-		url : "http://49.236.136.213:3000/web_admin/certificate",
+	 $.ajax({
+		url : "http://49.236.136.213:3000/request/change_flag",
 		type : "post",
-		data : admin,
+		data : request,
 		success : function(data) {
-			
-			if(data == 1){
-				alert("관리자 로그인에 성공하였습니다.");
-				$(location).attr("href", "member.html");
-			}
-			else {
-				alert("로그인 정보를 확인해주십시오." + data)
-			}
-			
+			//alert("수정 성공");
+			//location.reload();
 		},
 		error: function(request,status,error){
 			alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
 		}
 
-	}); */
+	});  
+
 }

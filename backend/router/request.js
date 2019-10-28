4c3dee4f5d3b891b8cbd6f99af80dfa0c4eae0c7
peter <peterhyun1234@gmail.com>
2019-10-28 19:45:40 +0900

    add some REST APIs

--- backend/router/request.js
+++ backend/router/request.js
@@ -7,7 +7,7 @@ var connection = require('../index.js').connection;
 router.get('/info', function (req, res) {
     connection.query('SELECT id, password from admin_user', function (err, data) {
         if (!err) {
-            //console.log(data);
+            console.log(data);
             res.send(data);
         }
         else {
@@ -20,26 +20,39 @@ router.get('/info', function (req, res) {
 router.post("/show_all_reqs", function (req, res, next) {
     var SQL = 'SELECT * FROM request';
 
-    //console.log(SQL);
+    console.log(SQL);
     //절 차 
     connection.query(SQL, function (err, data) {
-        console.log(data);
-        res.send(data);
+        if (!err) {
+            console.log(data);
+            res.send(data);
+        }
+        else {
+            console.log(err);
+            res.send('error');
+        }
     });
 });
 
 router.post("/change_flag", function (req, res, next) {
    
-    var recv_code = req.body.code;
-    var recv_flag = req.body.flag;
+    var recv_code = req.body.req_code;
+    var recv_flag = req.body.req_flag;
+    console.log('req_code: '+recv_code + 'req_flag: '+ recv_flag);
+    
+    var SQL = 'UPDATE request SET req_flag = ' + recv_flag + ' WHERE  req_code = '+ recv_code ;
 
-    var SQL = 'UPDATE request SET req_flag = \'' + recv_flag + 'WHERE code = \''+ recv_code +'\'';
-
-    //console.log(SQL);
+    console.log(SQL);
     //절 차 
     connection.query(SQL, function (err, data) {
-        console.log(data); //결과 뭐 나오는지 아직 잘 모르겠음
-        res.send(data);
+        if (!err) {
+            console.log(data);
+            res.send(data);
+        }
+        else {
+            console.log(err);
+            res.send('error');
+        }
     });
 });
 

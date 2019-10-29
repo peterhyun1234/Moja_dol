5608c960c419831a224ad4cc97324ed792cd0a01
peter <peterhyun1234@gmail.com>
2019-10-30 01:11:25 +0900

    add some APIs about review

--- backend/router/review.js
+++ backend/router/review.js
@@ -4,24 +4,27 @@ var router = express.Router();
 
 var connection = require('../index.js').connection;
 
-// router.get('/info', function (req, res) {
-//    connection.query('SELECT id, password from admin_user', function (err, data) {
-//       if (!err) {
-//          //console.log(data);
-//          res.send(data);
-//       }
-//       else {
-//          console.log(err);
-//          res.send('error');
-//       }
-//    });
-// });
+
+router.get('/:id', function (req, res) {
+   var policy_params = req.params.id;
+   //console.log('selected review is ' + policy_params);
+   connection.query('SELECT * from review where p_code = ' + policy_params, function (err, data) {
+       if (!err) {
+           console.log(data);
+           res.send(data);
+       }
+       else {
+           console.log(err);
+           res.send('error');
+       }
+   });
+});
 
 router.post("/selected_review", function (req, res, next) {
    var recv_code = req.body.p_code;
-   var SQL = 'SELECT * FROM review WHERE p_code = \'' + recv_code + '\'';
+   var SQL = 'SELECT * FROM review WHERE p_code = ' + recv_code ;
 
-   //console.log(SQL);
+   console.log(SQL);
    //console.log(recv_id + ", " + recv_password);
    //절 차 
    connection.query(SQL, function (err, data) {
@@ -36,5 +39,53 @@ router.post("/selected_review", function (req, res, next) {
       }
    });
 });
+
+router.post("/write_review", function (req, res, next) {
+   var recv_id = req.body.review_uID;
+   var recv_code = req.body.p_code;
+   var recv_comments = req.body.comments;
+   
+   var SQL = 'INSERT INTO PLAYER (p_code, review_uID, contents)'+
+   + 'VALUES (' +
+   + recv_code +
+   ',\'' + recv_id + '\'' +
+   ',\'' + recv_comments + '\'' +
+   ')';
+
+   console.log(SQL);
+   //console.log(recv_id + ", " + recv_password);
+   //절 차 
+   connection.query(SQL, function (err, data) {
+      //console.log(data);
+      if (!err) {
+         //console.log(data);
+         res.send(data);
+      }
+      else {
+         console.log(err);
+         res.send('error');
+      }
+   });
+});
+
+router.post("/delete_review", function (req, res, next) {
+    var recv_code = req.body.review_code;
+    var SQL = 'DELETE FROM review WHERE review_code = ' + recv_code ;
+ 
+    console.log(SQL);
+    //console.log(recv_id + ", " + recv_password);
+    //절 차 
+    connection.query(SQL, function (err, data) {
+       //console.log(data);
+       if (!err) {
+          //console.log(data);
+          res.send(data);
+       }
+       else {
+          console.log(err);
+          res.send('error');
+       }
+    });
+ });
 
 module.exports = router;

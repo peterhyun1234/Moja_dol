142c83aee5a0a89b0bacadda1f2db374f942570c
peter <peterhyun1234@gmail.com>
2019-11-08 23:03:10 +0900

    add Sppiner with custom UI

--- MYPOLICY/app/src/main/java/com/example/mypolicy/SearchActivity.java
+++ MYPOLICY/app/src/main/java/com/example/mypolicy/SearchActivity.java
@@ -11,6 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -46,10 +49,42 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     private String mClassName = getClass().getName().trim();
     private RecyclerView mRecyclerView;
 
+    private Spinner spinner2;
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> arrayAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+            arrayList = new ArrayList<>();
+            arrayList.add("철수");
+            arrayList.add("영희");
+            arrayList.add("람휘");
+            arrayList.add("녹지");
+            arrayList.add("치치");
+            arrayList.add("양가");
+            arrayList.add("용병");
+
+            arrayAdapter = new ArrayAdapter<>(getApplicationContext(),
+                    android.R.layout.simple_spinner_dropdown_item,
+                    arrayList);
+
+            spinner2 = (Spinner)findViewById(R.id.spinner2);
+            spinner2.setAdapter(arrayAdapter);
+            spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    Toast.makeText(getApplicationContext(),arrayList.get(i)+"가 선택되었습니다.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                }
+            });
+
 
         init();
         sharedPreferences = getSharedPreferences("session",MODE_PRIVATE);

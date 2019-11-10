eb0b30ad6f60ba1a068df9dbac7043dafcb5509b
peter <peterhyun1234@gmail.com>
2019-11-10 23:26:50 +0900

    Initiating search logic

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,7 @@ const requestRouter = require('./router/request');
 const reviewRouter=require('./router/review');
 const web_adminRouter=require('./router/web_admin');
 const my_listRouter=require('./router/my_list');
+const searchRouter=require('./router/search');
 
 //Route 정의
 app.use('/policy', policyRouter);
@@ -53,6 +54,7 @@ app.use('/request', requestRouter);
 app.use('/review', reviewRouter);
 app.use('/web_admin', web_adminRouter);
 app.use('/my_list', my_listRouter);
+app.use('/search', searchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/router/search.js
+++ backend/router/search.js
@@ -12,19 +12,27 @@ router.post("/test", function (req, res, next) {
 
     //1. 연령 분류(무조건 청년)
     //2. 성별 분류 필요 없음
+
+
     //3. 지역
+    // 지역 3~4 length의 경우 맨뒤 시, 군만 짤라서 ㄱㄱ
+    var recv_location = req.body.location;
+
     //4. 분야 (4개도 세분화해서 ㄱㄱ)
+        //4.0. 상관 없음.
         //4.1. Employment_Sup(취업지원) - 취업지원금, 서류/면접 지원, 취업지원 프로그램
         //4.2. Startup_sup(창업지원) - 시설/공간 제공, 정책 자금, 멘토링/컨설팅, 사업화
         //4.3. Life_welfare(생활, 복지) - 학자금, 대출/이자, 생활보조(금), 결혼/육아, 교통지원
         //4.4. Residential_financial(주거, 금융) - 기숙사/생활관, 주택지원, 주거환경지원, 고용장려금, 기업지원금
-    //5. 키워드 기반 검색 - title, contents내에 해당 키워드가 있으면 ㅇㅋ
-
+    //5. 연령
+        //4.1. Employment_Sup(취업지원) - 취업지원금, 서류/면접 지원, 취업지원 프로그램
+        //4.2. Startup_sup(창업지원) - 시설/공간 제공, 정책 자금, 멘토링/컨설팅, 사업화
+        //4.3. Life_welfare(생활, 복지) - 학자금, 대출/이자, 생활보조(금), 결혼/육아, 교통지원  
+    //6. 키워드 기반 검색 - title, contents내에 해당 키워드가 있으면 ㅇㅋ
     var recv_title = req.body.title;
     var recv_contents = req.body.contents;
 
     var recv_Atarget = req.body.application_target;
-    var recv_location = req.body.location;
     
     if(recv_code.length == 0) recv_code = null;
     if(recv_title.length == 0) recv_title = null;

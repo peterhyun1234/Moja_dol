b85ff060ba794f0312d910cc62f3bc1de7cabaaf
peter <peterhyun1234@gmail.com>
2019-11-20 17:42:18 +0900

    add mylist sorting standard of two cases

--- backend/router/my_list.js
+++ backend/router/my_list.js
@@ -65,6 +65,26 @@ router.post("/store_in_mylist", function (req, res, next) {
 
 router.post("/ordered_mylist", function (req, res, next) {
 
+    var recv_uID = req.body.uID;
+    var recv_Sort_by = req.body.Sort_by; 
+    // my List 정렬 기준 두가지 (type: String)
+    // 1. "저장날짜순"
+    // 2. "지원날짜순"
+
+    var sortingList = ["저장날짜순", "지원날짜순"];
+    var after_sortingList = ["s_p_time DESC", "apply_end is null ASC, apply_end ASC"];
+    
+    var ORDER_SQL = 'ORDER BY s_p_time DESC'; //default
+
+    console.log("sortingList.lenth: " + sortingList.length);
+
+    for (var i = 0; i < sortingList.length; i++) {
+        console.log("recv_Sort_by: " + recv_Sort_by);
+        if (recv_Sort_by == sortingList[i]) {
+            console.log("same: " + recv_Sort_by);
+            ORDER_SQL = "ORDER BY " + after_sortingList[i];
+        }
+    }
 
     var SQL = 'SELECT p_code, title, apply_start, apply_end '+
     'from stored_policy, policy ' + 

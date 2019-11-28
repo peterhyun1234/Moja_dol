b472d9eb4aea1a87a5c8edfbb20562b408bd320f
peter <peterhyun1234@gmail.com>
2019-11-29 00:17:24 +0900

    add registering user profile API after test

--- backend/router/user.js
+++ backend/router/user.js
@@ -52,7 +52,7 @@ router.post("/register", function (req, res, next) {
     // 사용자 사는 지역 (type: string)
     var recv_region_arr = req.body.region;
     var region_arr_len = recv_region_arr.length;
-    console.log("region_arr_len: " + region_arr_len);
+    //console.log("region_arr_len: " + region_arr_len);
     var recv_region;
 
     if (region_arr_len == 2 && recv_region_arr[1] != "") {
@@ -64,6 +64,12 @@ router.post("/register", function (req, res, next) {
     }
     else {
         recv_region = "미정";
+    }
+
+    // 사용자 성별 (type: String)
+    var recv_sex = req.body.sex;
+    if (recv_sex == null || recv_sex.length == 0) {
+        recv_sex = '';
     }
     // 사용자 나이 (type: integer)
     var recv_age = req.body.age;
@@ -99,22 +105,22 @@ router.post("/register", function (req, res, next) {
         '\'' + recv_uID + '\'' +
         ', \'' + recv_name + '\'' +
         ', \'' + recv_region + '\'' +
-        ', \'' + recv_sex + '\'' +
         ', ' + recv_age +
         ', ' + recv_Employment_sup_priority +
         ', ' + recv_Startup_sup_priority +
         ', ' + recv_Life_welfare_priority +
-        ', ' + recv_Residential_financial_priority + ') ' +
+        ', ' + recv_Residential_financial_priority +
+        ', \'' + recv_sex + '\'' + ') ' +
         "ON DUPLICATE KEY UPDATE " +
         "uID = '" + recv_uID + "', " +
         "name = '" + recv_name + "', " +
         "region = '" + recv_region + "', " +
-        "sex = '" + recv_sex + "', " +
         "age = " + recv_age + ", " +
         "Employment_sup_priority = " + recv_Employment_sup_priority + ", " +
         "Startup_sup_priority = " + recv_Startup_sup_priority + ", " +
         "Life_welfare_priority = " + recv_Life_welfare_priority + ", " +
-        "Residential_financial_priority = " + recv_Residential_financial_priority;
+        "Residential_financial_priority = " + recv_Residential_financial_priority + ", " +
+        "sex = '" + recv_sex + "'";
 
     console.log("API 'user/register' called");
     console.log(SQL);

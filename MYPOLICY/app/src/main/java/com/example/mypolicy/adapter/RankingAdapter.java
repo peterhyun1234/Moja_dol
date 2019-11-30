c34a0628b8cf05c3f4e162acb6fe63474ded54eb
장성범 <iwsl1234@naver.com>
2019-12-01 00:45:56 +0900

    클릭시 pcode에 맞게 top20->detail

--- MYPOLICY/app/src/main/java/com/example/mypolicy/adapter/RankingAdapter.java
+++ MYPOLICY/app/src/main/java/com/example/mypolicy/adapter/RankingAdapter.java
@@ -1,5 +1,7 @@
 package com.example.mypolicy.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.mypolicy.DetailPolicyActivity;
 import com.example.mypolicy.R;
 import com.example.mypolicy.model.Policy;
 import com.example.mypolicy.model.RankingData;
@@ -31,9 +34,20 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull RankingViewHolder holder, int position) {
         holder.title.setText(rList.get(position).getTitle());
-        long tmp=rList.get(position).getP_code();
+        final long pcode=rList.get(position).getP_code();
         
         holder.views.setText(Integer.toString(rList.get(position).getViews()));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context=view.getContext();
+                Intent intent=new Intent(context, DetailPolicyActivity.class);
+
+                intent.putExtra("position",pcode);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override

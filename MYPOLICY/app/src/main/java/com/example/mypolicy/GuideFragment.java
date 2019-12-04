2cb5569d92aeb21530ec7619ed3dcae9bc966794
장성범 <iwsl1234@naver.com>
2019-12-05 02:37:00 +0900

    in fragment click event

--- MYPOLICY/app/src/main/java/com/example/mypolicy/GuideFragment.java
+++ MYPOLICY/app/src/main/java/com/example/mypolicy/GuideFragment.java
@@ -1,5 +1,8 @@
 package com.example.mypolicy;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,12 +11,25 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.example.mypolicy.model.Policy;
 import com.example.mypolicy.model.Referral;
+import com.example.mypolicy.service.IApiService;
+import com.example.mypolicy.service.RestClient;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import es.dmoral.toasty.Toasty;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GuideFragment extends Fragment {
 
@@ -21,6 +37,10 @@ public class GuideFragment extends Fragment {
     private Referral referral;
     private ImageView imageView;
     private TextView textView;
+    final IApiService iApiService=new RestClient("http://49.236.136.213:3000/").getApiService();
+    final HashMap<String,Object> clickMap=new HashMap<>();
+
+    SharedPreferences sharedPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,8 @@ public class GuideFragment extends Fragment {
         //bgRes = getArguments().getInt("data");
         referral = (Referral) getArguments().get("data");
 //        name=getArguments().getString("data");
+
+
 
     }
     @Override
@@ -42,9 +64,34 @@ public class GuideFragment extends Fragment {
 
 
         //imageView = (ImageView) getView().findViewById(R.id.image);
-
         //imageView.setBackgroundResource(bgRes);
+
         textView=getView().findViewById(R.id.tv_policy_name_test);
         textView.setText(referral.getTitle());
+        Log.d("체코드",""+referral.getP_code());
+      //  final Call<ArrayList<Policy>> call=iApiService.showselectedPolicy(R.id.tv_policy_pcode_test);
+
+//        Log.d("체코드",""+GuideFragment.this.getActivity().getSharedPreferences("userEmail", Context.MODE_PRIVATE));
+//        clickMap.put("uID",share);
+        clickMap.put("p_code",referral.getP_code());
+//        final Call<JsonObject> clickCall=iApiService.clickPolicy()
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                call.clone().enqueue(new Callback<ArrayList<Policy>>() {
+                    @Override
+                    public void onResponse(Call<ArrayList<Policy>> call, Response<ArrayList<Policy>> response) {
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ArrayList<Policy>> call, Throwable t) {
+
+                    }
+                });
+
+            }
+        });
+
     }
 }

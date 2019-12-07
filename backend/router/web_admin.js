69fbf30f2d2cac859a69594b0b183d5793dd8560
peter <peterhyun1234@gmail.com>
2019-12-07 23:33:26 +0900

    add API for handling session

--- backend/router/web_admin.js
+++ backend/router/web_admin.js
@@ -22,11 +22,6 @@ router.post("/certificate", function (req, res, next) {
    var recv_password = req.body.password;
 
    const IP = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-   //console.log("IP: " + IP);
-   // 로그인하면 admin_user에 ip등록
-   // --로그인 안하고 web_certificate하면 user에 등록된 ip가 아니기 때문에 거절
-   // 등록된 ip는 3시간 뒤 자동으로 0
-   // 로그아웃 시 자동으로 0으로 초기화
 
    var SQL = "UPDATE admin_user " +
    "SET id = '" + recv_id + "', password = '" + recv_password + "', login_IP = '" + IP + "'" +
@@ -60,10 +55,35 @@ router.post("/session_certificate", function (req, res, next) {
    //절 차 
    connection.query(SQL, function (err, data) {
       //console.log(data);
-      if (data.length == '1') {
+      if (data.length != 0) {
+        // console.log("성공");
          res.send('1');
       }
       else {
+        // console.log("실패");
+         res.send('-1');
+      }
+   });
+});
+
+router.post("/logout", function (req, res, next) {
+
+
+   const IP = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
+
+   var SQL = "UPDATE admin_user SET login_IP = '0' WHERE login_IP = '" + IP + "'";
+
+   console.log(SQL);
+   //console.log(recv_id + ", " + recv_password);
+   //절 차 
+   connection.query(SQL, function (err, data) {
+      //console.log(data);
+      if (data.length != 0) {
+        // console.log("성공");
+         res.send('1');
+      }
+      else {
+        // console.log("실패");
          res.send('-1');
       }
    });
